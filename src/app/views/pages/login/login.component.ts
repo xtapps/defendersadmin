c27ccdcd295794b6391bfb17f68ed2a96c596604
Maria kishore <51889523+kishore-maria@users.noreply.github.com>
2023-10-13 00:04:57 +0530

    Login functionality added

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   authForm!: FormGroup;
   isInvalid = false;
+  subscriptions: Subscription[] = [];
 
   constructor(
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private adminService: AdminService
   ) { }
 
   ngOnInit(): void {
@@ -30,13 +34,41 @@ export class LoginComponent implements OnInit {
 
   login(): void {
     const formValue = this.authForm.value;
-    if (formValue.username === 'nithil.defendersGateway.com' && formValue.password === '123456') {
-      this.isInvalid = false;
-      this.router.navigateByUrl('/dashboard');
-    } else {
+    if (!formValue.username || !formValue.password) {
       this.isInvalid = true;
+      return;
     }
+    const data = {
+      email: formValue.username,
+      password: formValue.password,
+      device: '',
+      deviceBrand: ''
+    }
+    const loginSub = this.adminService.login(data).subscribe(res => {
+      localStorage.setItem('token', res.response.token);
+      const userDetails = {
+        firstName: res.response.firstName,
+        lastName: res.response.lastName,
+        email: res.response.email,
+        userID: res.response.userID
+      }
+      localStorage.setItem('userDetails', JSON.stringify(userDetails));
+      this.router.navigateByUrl('/dashboard');
+    }, err => {
+      console.log(err);
+    });
+    this.subscriptions.push(loginSub);
+    // if (formValue.username === 'nithil.defendersGateway.com' && formValue.password === '123456') {
+    //   this.isInvalid = false;
+    //   this.router.navigateByUrl('/dashboard');
+    // } else {
+    //   this.isInvalid = true;
+    // }
 
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
 }
