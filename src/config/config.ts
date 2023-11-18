8d165f3c95455aef1223200de6582b58370f75f2
Maria kishore <51889523+kishore-maria@users.noreply.github.com>
2023-11-19 02:10:45 +0530

    s3 url added

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
 // export const apiUrl = 'http://localhost:1111';
 // export const apiUrl = 'http://198.199.67.118:1111';
 export const apiUrl = 'https://apiv2.defenders-gateway.com';
+export const s3Url = 'https://dg-partner-photos.s3.amazonaws.com/photos/';
