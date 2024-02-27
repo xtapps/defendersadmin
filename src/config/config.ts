a234bf318e2fa79cf0a1852c317089e978acd7e9
Maria kishore <51889523+kishore-maria@users.noreply.github.com>
2024-02-27 23:26:27 +0530

    admin page not loading issue fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,5 +2,5 @@
 // export const apiUrl = 'http://198.199.67.118:1111';
 export const apiUrl = 'https://apiv2.defenders-gateway.com';
 export const s3Url = 'https://dg-partner-photos.s3.amazonaws.com/photos/';
-export const adminUrl = 'adminv2.defenders-gateway.com/';
+export const adminUrl = 'adminv2.defenders-gateway.com';
 export const partnerUrl = 'partner.defenders-gateway.com';
