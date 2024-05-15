001796f054454c7b8ef41a6e0914431f0999d896
Maria kishore <51889523+kishore-maria@users.noreply.github.com>
2024-05-15 22:32:45 +0530

    Side menu highlight and text correction done

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { navItems } from './_nav';
 
@@ -7,9 +8,47 @@ import { navItems } from './_nav';
   templateUrl: './default-layout.component.html',
   styleUrls: ['./default-layout.component.scss'],
 })
-export class DefaultLayoutComponent {
+export class DefaultLayoutComponent implements OnInit {
 
   public navItems = navItems;
 
-  constructor() {}
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.highlightSideMenu()
+    }, 500);
+    // this.router.events.subscribe((event: any) => {
+    //   if (event instanceof NavigationEnd) {
+    //     setTimeout(() => {
+    //       this.highlightSideMenu()
+    //     }, 500);
+    //   }
+    // });
+  }
+
+  highlightSideMenu() {
+    const curUrl = this.router.url;
+    this.navItems.forEach((item, idx) => {
+      const title = document.getElementsByTagName('c-sidebar-nav-group')[idx]?.getElementsByTagName('a')[0].text;
+      if (item.url === curUrl) {
+        this.updateElement(idx);
+      } else {
+        item.children?.forEach(child => {
+          if (child.url === curUrl) {
+            this.updateElement(idx);
+          }
+        })
+      }
+    })
+  }
+
+  updateElement(idx: number) {
+    if (idx <= 0) return;
+    const navGrp = document.getElementsByTagName('c-sidebar-nav-group')[idx - 1];
+    navGrp.classList.add('show');
+    const sideNav = document.getElementsByTagName('c-sidebar-nav-group')[idx - 1].getElementsByTagName('c-sidebar-nav')[0];
+    sideNav.setAttribute('style', 'height: auto; display: block;')
+  }
+
 }
